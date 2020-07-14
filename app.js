15543b17514cb7cca6fc223aa42b143a69d6e6bd
Toshiki Isawa <jack_the_fool@outlook.com>
2020-07-14 16:10:12 +0900

    Rock-paper-scissors

--- app.js
+++ app.js
@@ -53,6 +53,10 @@
 
 // じゃんけんの手を入力してもらうプロンプト欄を生成
 var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー")&&(user_hand != "チョキ")&&(user_hand != "パー")&&(user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 
 // じゃんけんの手をランダムに作成する関数を呼び出す
 var js_hand = getJShand();
@@ -61,7 +65,11 @@ var js_hand = getJShand();
 var judge = winLose(user_hand, js_hand);
 
 // 結果を表示する
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if(user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
 
 // ランダムでじゃんけんの手を作成する関数
 function getJShand(){
